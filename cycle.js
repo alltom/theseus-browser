ecce7361b9cb889d5ef5e05b8ffacf235daffd39
Tom Lieber <tom@alltom.com>
2013-07-11 15:04:37 -0700

    moved Cycle to a separate .js file

---
+++ cycle.js
@@ -0,0 +1,11 @@
+function Cycle(values) {
+	this._values = values;
+	this._next = 0;
+}
+Cycle.prototype = {
+	next: function () {
+		var value = this._values[this._next];
+		this._next = (this._next + 1) % this._values.length;
+		return value;
+	}
+};
